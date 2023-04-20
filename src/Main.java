9d82b1c9228c57cee55dae5e02efdb5ee676147c
Vladi0404 <moskva.vladi@yandex.ru>
2023-04-20 12:37:25 +0200

    Finale Home Work

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,66 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    // task 1
+    public static void printYear(int finalResult) {
+        if (finalResult % 4 == 0 && finalResult % 100 != 0 || finalResult % 400 == 0)
+            System.out.println(finalResult + " год является высокосным.");
+        else if (finalResult % 4 != 0 && finalResult % 100 == 0 || finalResult % 400 != 0) {
+            System.out.println(finalResult + " год не является высокосным.");
+        }
     }
-}
+
+    // task 2
+    public static void mobile (int clientOS1, int clientDeviceYear1) {
+        if (clientOS1 == 0 && clientDeviceYear1 >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS1 == 0 && clientDeviceYear1 < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientOS1 == 1 && clientDeviceYear1 >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS1 == 1 && clientDeviceYear1 < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
+
+    // task 3
+    public static void distance (int distance1){
+        if ( distance1 < 20) {
+            System.out.println("Потребуется 1 день для доставки.");
+        }
+        else if ( distance1 >= 20 && distance1 < 60) {
+            System.out.println("Потребуется 2 дня для доставки.");
+        }
+        else if ( distance1 >= 60 && distance1 < 100) {
+            System.out.println("Потребуется 3 дня для доставки.");
+        } else {
+            System.out.println("Свыше 100 км доставки нет.");
+        }
+
+    }
+
+
+        public static void main (String[]args){
+
+            // task 1
+            short year = 2023;
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                printYear(year);
+            } else {
+                printYear(year);
+            }
+
+            // task 2
+
+            int clientOS = 0;
+            int clientDeviceYear = 2010;
+            mobile (clientOS, clientDeviceYear);
+
+            // task 3
+
+            int deliveryDistance = 74;
+            distance(deliveryDistance);
+
+
+        }
+    }
